7a565279015a5a485d4ea52ff85904ef853cf5da
lzj <675157499@qq.com>
2018-05-14 22:26:19 +0800

    开始用户管理页面

---
+++ public/manage_my/js/base.js
@@ -0,0 +1,40 @@
+$(function () {
+    //  判断是否登录
+    $.ajax({
+        url: '/employee/checkRootLogin',
+        success: function (backData) {
+            if (backData.error == 400) {
+                window.location.href = './login.html'
+            } else {
+                //转义符号是关键
+                // $('body').append('<script src="./js/index.js"><\/script>');
+                $.getScript('./js/index.js')
+            }
+        }
+    })
+     
+    $('.lt_aside .content ul').children('li').click(function () {
+        console.log($(this)); 
+        $(this).toggleClass('now')
+    });
+    $('.lt_main .header a').first().click(function () {
+        // console.log('1');
+        $('.lt_aside').toggle()
+        $('.lt_main').toggleClass('full_screen')
+    });
+    // 弹出模态框
+    $('a.glyphicon-log-out').click(function () {
+        $('#myModal').modal('show')
+    })
+    //隐藏模态框
+    $('.modal-comfirm .btn-primary').click(function () {
+
+        $('#myModal').modal('hide');
+        $.ajax({
+            url: '/employee/employeeLogout',
+            success: function (backData) {
+                window.location.href = "./login.html"
+            }
+        })
+    })    
+})
